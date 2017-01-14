40db569b127cf9d8d9c7318e8d1e86e35c0ac966
Christoph Kappestein <k42b3.x@gmail.com>
2017-01-14 23:54:04 +0100

    remove http and cache

--- src/Action/V8Processor.php
+++ src/Action/V8Processor.php
@@ -59,8 +59,6 @@ class V8Processor extends ActionAbstract
         $environment->set('context', new Wrapper\Context($context));
         $environment->set('connector', new Wrapper\Connector($this->connector));
         $environment->set('processor', new Wrapper\Processor($this->processor, $context));
-        $environment->set('http', new Wrapper\Http\Client($this->httpClient));
-        $environment->set('cache', new Wrapper\Cache\Provider($this->cacheProvider));
 
         try {
             $environment->run($configuration->get('code'));
